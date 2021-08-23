a35f9320eefec273dfa143de35767ed959141b8b
charun umesh <charunumesh@gmail.com>
2021-08-23 23:52:08 +0530

    scraping started

--- scrapingapp/views.py
+++ scrapingapp/views.py
@@ -1,11 +1,14 @@
+import requests
 from django.shortcuts import render
 from bs4 import BeautifulSoup
-import requests
+import urllib3
+from urllib.parse import quote_plus
 
 # Create your views here.
 
 # create home views
 
+BASE_URL = 'https://kerala.craigslist.org/d/events-classes/search/eee?query={}'
 
 def home(request):
     return render(request, 'base.html')
@@ -13,5 +16,8 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')
+    final_url = BASE_URL.format(quote_plus(search))
+    print(final_url)
+    response = requests.get(final_url)
     frontend_stuff = {'search': search}
     return render(request, 'scrapingapp/new_search.html', frontend_stuff)
