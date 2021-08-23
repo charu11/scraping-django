d5c6d028eb9c3656c1c4f7988ee5a046d5f97a6b
charun umesh <charunumesh@gmail.com>
2021-08-23 20:48:33 +0530

    template added

--- scrapingapp/urls.py
+++ scrapingapp/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 app_name = 'scraping'
 urlpatterns = [
+    path('new_search/', views.new_search, name='new_search'),
     path('', views.home, name='home')
+
 ]
 
--- scrapingapp/views.py
+++ scrapingapp/views.py
@@ -7,3 +7,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'base.html')
+
+
+def new_search(request):
+    return render(request, 'scrapingapp/new_search.html')
